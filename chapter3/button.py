ba2d0bdcb644dcdd7e589941c9dce279bdc32021
Jimmy McNamara <jmcnamara44@gmail.com>
2018-12-17 22:50:59 -0800

    create a file that displays a clikcable button.

---
+++ chapter3/button.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+window = tk.Tk()
+
+def buttonClick():
+    button.config(text="Clicked")
+    
+button = tk.Button(window, text="Click me", command=buttonClick)
+button.pack()
+window.mainloop()
